bab0b2aeb8416dabf79ae1926b1781f9fb8378ab
Jacob Clark <jacob.jh.clark@googlemail.com>
2020-06-01 12:41:14 +0100

    Format output

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ async function loadWasm(path) {
   return module.instance.exports;
 }
 
+const formatForDisplay = x => {
+  return {  
+      "-1": "Fizz",
+      "-2": "Buzz",
+      "-3": "FizzBuzz"
+    }[String(x)]
+}
+
 loadWasm('./fizzbuzz.wasm').then(x => {
-  console.log(3, x.fizzbuzz(3)); // -1 | fizz
-  console.log(6, x.fizzbuzz(6)); // -1 | fizz
-  console.log(5, x.fizzbuzz(5)); // -2 | buzz
-  console.log(10, x.fizzbuzz(10)); // -2 | buzz
-  console.log(15, x.fizzbuzz(15)); // -3 | fizzbuzz
+  document.writeln("3 is " + formatForDisplay(x.fizzbuzz(3)) + "<br/>");
+  document.writeln("6 is " + formatForDisplay(x.fizzbuzz(6)) + "<br/>");
+  document.writeln("5 is " + formatForDisplay(x.fizzbuzz(5)) + "<br/>"); 
+  document.writeln("10 is " + formatForDisplay(x.fizzbuzz(10)) + "<br/>");
+  document.writeln("15 is " + formatForDisplay(x.fizzbuzz(15)) + "<br/>");
 })
